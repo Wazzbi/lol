4584f96bf6d828ed985fac135324cb2c65cdfb42
David Novotny <davidnovotny47@gmail.com>
2020-02-13 22:06:08 +0100

    prepered for curr.match

--- src/app/containers/spectator/spectator.component.ts
+++ src/app/containers/spectator/spectator.component.ts
@@ -12,8 +12,25 @@ export class SpectatorComponent implements OnInit {
   summoner$: Observable<Summoner> = null;
   name: string = "";
   showTabs: boolean = false;
+  acTab: Number = 0;
 
-  links = ["First", "Second", "Third"];
+  links = [
+    {
+      label: "First",
+      index: "1"
+      //path: "/currentMatch"
+    },
+    {
+      label: "Second",
+      index: "2"
+      //path: "/currentMatch"
+    },
+    {
+      label: "Third",
+      index: "3"
+      //path: "/currentMatch"
+    }
+  ];
   activeLink = this.links[0];
 
   constructor(private specService: SpectatorService) {}
@@ -25,5 +42,9 @@ export class SpectatorComponent implements OnInit {
     if (this.summoner$ !== null) {
       this.showTabs = true;
     }
+  }
+
+  activeTab(index: number) {
+    this.acTab = index;
   }
 }
